0f4a88f080617d6563406cbbcbe4e1e14ded8880
Takuya Mannami <mtakuya@users.sourceforge.jp>
2009-11-04 07:03:24 +0900

    add test

---
+++ test.scm
@@ -0,0 +1,8 @@
+(use gauche.test)
+(test-start "twig (v0.0.2)")
+(load "./test/test1.scm")
+(test-module 'twig)
+(test-module 'rfc.http)
+(test-module 'rfc.uri)
+(test-module 'rfc.base64)
+(test-end)
